990999482fb994921d8ef3c65dca55629cca1efc
Otávio Oliveira <59941437+S4vant@users.noreply.github.com>
2023-02-24 21:45:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("Olá!")
+
+const a = Number(prompt("Digite o valor do salário"))
+const b = Number(prompt("Digite a porcentagem de aumento"))
+
+//const lista  = [a,b]
+let resultado =a+ (a * (b/100))
+console.log(resultado)
+
+
+document.write(resultado)
